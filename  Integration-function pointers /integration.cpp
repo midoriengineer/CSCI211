21b6381dad8dc2e0b8c0617a3b069f0019195b28
midoriengineer <115385098+midoriengineer@users.noreply.github.com>
2022-12-08 01:21:47 -0500

    Update integration.cpp

---  Integration-function pointers /integration.cpp
+++  Integration-function pointers /integration.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+//FUNC represents functions of one variable that take a double as input and returns a double
+typedef double (*FUNC)(double);
+
+//finds the sum (area) of the all the small rectangles under the function f between the points a and b
+//where the width of each rectangle is .0001. Then, it returns that sum.
+double integrate(FUNC f, double a, double b){
+  double sum =0;
+
+  for(int i = a; i < b; i+= .0001){
+    sum = sum + (.0001)*f(i);
+  }
+  return sum;
+}
+
 double line(double x){
 return x;
 {
@@ -14,11 +28,6 @@ return x*x*x;
 {
 
 int main(){
-
-  //FUNC represents functions of one variable that take a double as input and returns a double
-  typedef double (*FUNC)(double);
-  double integrate(FUNC f, double a, double b);
-
 
   cout<< “The integral of f(x)=x between 1 and 5 is: “<<integrate(line, 1, 5)<<endl;
   cout<< “The integral of f(x)=x^2 between 1 and 5 is: “<<integrate(square, 1, 5)<<endl;
