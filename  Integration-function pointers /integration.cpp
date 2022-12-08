d5dfcdce423f4108c1488aa9ef6d175f8eba90c8
midoriengineer <115385098+midoriengineer@users.noreply.github.com>
2022-12-08 01:08:55 -0500

    upload integration

---
+++  Integration-function pointers /integration.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+double line(double x){
+return x;
+{
+
+double square(double x){
+return x*x;
+{
+
+double cube(double x){
+return x*x*x;
+{
+
+int main(){
+
+  //FUNC represents functions of one variable that take a double as input and returns a double
+  typedef double (*FUNC)(double);
+  double integrate(FUNC f, double a, double b);
+
+
+  cout<< “The integral of f(x)=x between 1 and 5 is: “<<integrate(line, 1, 5)<<endl;
+  cout<< “The integral of f(x)=x^2 between 1 and 5 is: “<<integrate(square, 1, 5)<<endl;
+  cout<< “The integral of f(x)=x^3 between 1 and 5 is: “<<integrate(cube, 1, 5)<<endl;
+
+
+  return 0;
+}
