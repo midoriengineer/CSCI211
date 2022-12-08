a4ac3efa06cea14bed3ecaaaf97ae5fc010d8416
midoriengineer <115385098+midoriengineer@users.noreply.github.com>
2022-12-08 01:38:06 -0500

    update integration

---  Integration-function pointers /integration.cpp
+++  Integration-function pointers /integration.cpp
@@ -9,29 +9,29 @@ typedef double (*FUNC)(double);
 double integrate(FUNC f, double a, double b){
   double sum =0;
 
-  for(int i = a; i < b; i+= .0001){
+  for(double i = a; i < b; i+= .0001){
     sum = sum + (.0001)*f(i);
   }
   return sum;
 }
 
 double line(double x){
-return x;
-{
+  return x;
+}
 
 double square(double x){
-return x*x;
-{
+  return x*x;
+}
 
 double cube(double x){
-return x*x*x;
-{
+  return x*x*x;
+}
 
 int main(){
 
-  cout<< “The integral of f(x)=x between 1 and 5 is: “<<integrate(line, 1, 5)<<endl;
-  cout<< “The integral of f(x)=x^2 between 1 and 5 is: “<<integrate(square, 1, 5)<<endl;
-  cout<< “The integral of f(x)=x^3 between 1 and 5 is: “<<integrate(cube, 1, 5)<<endl;
+  cout<< "The integral of f(x)=x between 1 and 5 is: " << integrate(line, 1, 5) <<endl;
+  cout<< "The integral of f(x)=x^2 between 1 and 5 is: " << integrate(square, 1, 5) <<endl;
+  cout<< "The integral of f(x)=x^3 between 1 and 5 is: " << integrate(cube, 1, 5) <<endl;
 
 
   return 0;
